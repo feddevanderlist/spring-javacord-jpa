5ceda8a5a344e87dc688d914da7e9252e56e95ba
vandalist <feddevanderlist@gmail.com>
2023-09-09 09:02:28 +0200

    slashcommandExample added

--- src/main/java/com/example/springbootjavacord/configuration/DiscordApiConfiguration.java
+++ src/main/java/com/example/springbootjavacord/configuration/DiscordApiConfiguration.java
@@ -1,14 +1,20 @@
 package com.example.springbootjavacord.configuration;
 
 import com.example.springbootjavacord.services.ExampleListener;
+import com.example.springbootjavacord.services.ExampleSlashCommandListener;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandBuilder;
+import org.javacord.api.interaction.SlashCommandOption;
+import org.javacord.api.interaction.SlashCommandOptionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,6 +28,8 @@ public class DiscordApiConfiguration {
     @Autowired
     private ExampleListener exampleListener;
 
+    @Autowired
+    private ExampleSlashCommandListener exampleSlashCommandListener;
     private final Logger LOGGER = Logger.getLogger(this.getClass().toString());
 
     @Bean
@@ -29,7 +37,13 @@ public class DiscordApiConfiguration {
         String token = env.getProperty("TOKEN");
         DiscordApiBuilder builder = new DiscordApiBuilder().setToken(token);
         DiscordApi api = builder.setAllNonPrivilegedIntents().login().join();
+
+        api.bulkOverwriteGlobalApplicationCommands(Set.of(
+                new SlashCommandBuilder().setName("example").setDescription("exampleCommand").addOption(SlashCommandOption.create(SlashCommandOptionType.STRING, "message", "Your message", true))
+        ));
+
         api.addListener(exampleListener);
+        api.addListener(exampleSlashCommandListener);
         sendMessageToOwnerOnStartUp(api);
         return api;
     }
---
+++ src/main/java/com/example/springbootjavacord/services/ExampleSlashCommandListener.java
@@ -0,0 +1,31 @@
+package com.example.springbootjavacord.services;
+
+import com.example.springbootjavacord.model.ExampleModel;
+import com.example.springbootjavacord.repository.ExampleRepository;
+import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.event.interaction.SlashCommandCreateEvent;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.callback.InteractionOriginalResponseUpdater;
+import org.javacord.api.listener.interaction.SlashCommandCreateListener;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ExampleSlashCommandListener
+        implements SlashCommandCreateListener {
+
+    @Autowired
+    ExampleRepository exampleRepository;
+
+    @Override
+    public void onSlashCommandCreate(final SlashCommandCreateEvent event) {
+        final SlashCommandInteraction interaction = event.getSlashCommandInteraction();
+        //optional check might be handy when you have more commands :)
+        if (interaction.getCommandName().equals("example")) {
+            CompletableFuture<InteractionOriginalResponseUpdater> cfInteractionUpdater = interaction.createImmediateResponder().setContent("this is a response to your command").setFlags(MessageFlag.EPHEMERAL).respond();
+
+            exampleRepository.save(new ExampleModel(event.getSlashCommandInteraction().getArgumentStringRepresentationValueByName("message").orElseThrow(NullPointerException::new)));
+            cfInteractionUpdater.thenAccept(interactionOriginalResponseUpdater -> interactionOriginalResponseUpdater.setContent("message update: your message has been saved tot he database").update());
+        }
+    }
+}
